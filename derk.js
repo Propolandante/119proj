85e8ddb41dd9a986795a4db7332ef5149327437e
Derek Donahue <propolandante@gmail.com>
2014-05-12 20:16:28 -0700

    Displays image in center of screen at 100%

--- derk.js
+++ derk.js
@@ -1,5 +1,13 @@
-var path = new Path();
-path.strokeColor = 'red';
-var start = new Point(200, 200);
-path.moveTo(start);
-path.lineTo(start + [100, -40]);
+//derk.js
+
+var img = document.createElement("img");
+img.src = "1.jpg";
+img.id = "current_image";
+document.body.appendChild(img);
+
+
+// Create a raster item using the image tag with id='mona'
+var raster = new Raster('current_image');
+
+// Move the raster to the center of the view
+raster.position = view.center;
