98a4a84ee2b9350ae4903462c302defb842bdaff
Derek Donahue <propolandante@gmail.com>
2014-05-12 21:49:37 -0700

    Image is displayed in center of screen, button allows user to go to next image. After cycling through all ten images, it loops back to the first

--- derk.js
+++ derk.js
@@ -1,13 +1,60 @@
 //derk.js
 
-var img = document.createElement("img");
-img.src = "1.jpg";
-img.id = "current_image";
-document.body.appendChild(img);
+var img_number = 0;
+var img_count = 10; // total number of images in directory
+var img_directory = "http://people.ucsc.edu/~djdonahu/119proj/images/";
 
 
-// Create a raster item using the image tag with id='mona'
-var raster = new Raster('current_image');
+var raster;
 
-// Move the raster to the center of the view
-raster.position = view.center;
+loadNextImage();
+
+// press enter to go to next image
+// function onKeyDown(event) {
+	// if (event.key == "enter")
+	// {
+		// loadNextImage();
+	// }
+// 	
+// }
+
+//clicking the button loads the next image
+document.getElementById("nextImage").onclick = loadNextImage;
+
+
+
+function loadNextImage() {
+	
+	if(raster != null) // if there is already an image
+	{
+		
+		raster.remove(); // remove it
+	}
+	
+	//create a standard JavaScript image
+	var img = document.createElement("img");
+	document.body.appendChild(img);
+	
+	///increment counter to load next image
+	if (img_number < img_count)
+	{
+		img_number++;
+	}
+	else
+	{
+		img_number = 1;
+	}
+	//load JavaScript image from source
+	img.src = img_directory + img_number + ".jpg";
+	img.id = "image_" + img_number;
+	
+	//create PaperJS raster of JavaScript image
+	raster = new Raster(img.id);
+	
+	// get rid of JavaScript image
+	img.remove();
+	
+	//position the raster
+	raster.position.x = view.center.x;
+	raster.position.y = raster.height/2 + 10;
+};
