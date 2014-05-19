047ec18a8999cdc830e5b63225c7ec5e6b51ad4b
huanghunz <huanghunz@hotmail.com>
2014-05-19 16:58:36 -0700

    1.major changes in function  zxcMakeTextBox of derk_level_2.js ,   added userDefine (global array)   define opacity with  --- var normalOpacity = 0.85;                                       --- var typingOpacity = 0.55;
    2.test_with_jquery.html adds library about jquery and autocomplete function
    
    3. in auto_stlye.css,  .text-field is not used.
    
    4. words_source contains one arrary

---
+++ words_source.js
@@ -0,0 +1,14 @@
+var sources = [
+      "Apple",
+      "Animal",
+      "Angry Bird",
+      "Bee",
+      "Brittany",
+      "Cat",
+      "Dog",
+      "Derek",
+      "Jiayu",
+      "Person",
+      "River",
+      "Rock",
+    ];
