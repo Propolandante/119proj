fdd3d124a8d9437ccc99676627957c8eaa80dcc6
Derek Donahue <propolandante@gmail.com>
2014-05-20 10:11:34 -0700

    Edited the word banks to be more useful, adding a readme

---
+++ objects_source.js
@@ -0,0 +1,12 @@
+var sources = [
+      "brittany",
+      "derek",
+      "jiayu",
+      "man",
+      "person",
+      "river",
+      "rock",
+      "sky",
+      "tree",
+      "woman"
+    ];
---
+++ relationships_source.js
@@ -0,0 +1,12 @@
+var sources = [
+      "above",
+      "behind",
+      "below",
+      "holding",
+      "in",
+      "in front of",
+      "inside",
+      "next to",
+      "on",
+      "talking to"
+    ];
